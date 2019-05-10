77fe5010ade8e5596164f993e815a5cb520cd43e
Kyle Franke <kfranke1@binghamton.edu>
2019-05-10 14:41:38 -0400

    Bars work

---
+++ Assets/Unity UI Samples/Scripts/DecreaseHealth.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DecreaseHealth : MonoBehaviour
+{
+
+    public void Run(int checksum) {
+        int player = 1;
+        if (checksum < 0) {
+            player = -1;
+        }
+        int damage = Mathf.Abs(checksum);
+
+        GameObject bar;
+        GameObject health;
+        if (player == 1) {
+            bar = GameObject.Find("Canvas/Player1Health");
+            health = GameObject.Find("Canvas/Player1Health/Health1");
+        } else {
+            bar = GameObject.Find("Canvas/Player2Health");
+            health = GameObject.Find("Canvas/Player2Health/Health2");
+        }
+        
+        Vector3 pos = bar.transform.position;
+        health.transform.localScale -= new Vector3((float) damage/100, 0, 0);
+        
+        Vector3 pos2 = health.transform.position;
+        float change = (float) (player * (.35 * ((float) damage/100)));
+        Debug.Log(change);
+        health.transform.localPosition -= new Vector3(change, 0, 0);
+    }
+
+}
